e9d57302ddabe5c78a864688d0dfb29623cbe6a4
d0u9 <d0u9.su@outlook.com>
2016-10-05 15:12:15 +0800

    Refactor event_handler.c

--- event_handler.c
+++ event_handler.c
@@ -8,55 +8,56 @@ gboolean system_monitor_size_changed(XfcePanelPlugin *plugin, gint size);
 gboolean timeout(sys_monitor_t *base);
 void system_monitor_free(XfcePanelPlugin *plugin, sys_monitor_t *base);
 
-
 gboolean system_monitor_size_changed(XfcePanelPlugin *plugin, gint size)
 {
-        GtkOrientation orientation = xfce_panel_plugin_get_orientation(plugin);
+	GtkOrientation orientation = xfce_panel_plugin_get_orientation(plugin);
 
-        if (orientation == GTK_ORIENTATION_HORIZONTAL)
-                gtk_widget_set_size_request(GTK_WIDGET(plugin), -1, -1);
-        else
-                gtk_widget_set_size_request(GTK_WIDGET(plugin), size, -1);
+	if (orientation == GTK_ORIENTATION_HORIZONTAL)
+		gtk_widget_set_size_request(GTK_WIDGET(plugin), -1, -1);
+	else
+		gtk_widget_set_size_request(GTK_WIDGET(plugin), size, -1);
 
-        return TRUE;
+	return TRUE;
 }
-
 
 gboolean timeout(sys_monitor_t *base)
 {
-        gchar str[32] = {0};
+	gchar str[32] = {0};
 
-        /* update cpu usage */
-        update_cpu(&base->cpu);
-        snprintf(str, 31, "%.1f%%", base->cpu.total.load / 100.0);
-        gtk_label_set_text(GTK_LABEL(base->gui.cpu_usage_label), str);
+	/* update cpu usage */
+	update_cpu(&base->cpu);
+	snprintf(str, 31, "%.1f%%", base->cpu.total.load / 100.0);
+	gtk_label_set_text(GTK_LABEL(base->gui.cpu_usage_label), str);
 
-        /* update network */
-        update_net(&base->net);
-        update_speed_str(&base->net, get_mseconds_by_level(base->update_interval));
-        g_print("send = %s, recv = %s\n", base->net.send_speed, base->net.recv_speed);
-        gtk_label_set_text(GTK_LABEL(base->gui.dowlink_speed_label), base->net.recv_speed);
-        gtk_label_set_text(GTK_LABEL(base->gui.uplink_speed_label), base->net.send_speed);
+	/* update network */
+	update_net(&base->net);
+	update_speed_str(&base->net, get_mseconds_by_level(base->update_interval));
+	g_print("send = %s, recv = %s\n",
+		base->net.send_speed, base->net.recv_speed);
+	gtk_label_set_text(GTK_LABEL(base->gui.dowlink_speed_label),
+			   base->net.recv_speed);
+	gtk_label_set_text(GTK_LABEL(base->gui.uplink_speed_label),
+			   base->net.send_speed);
 
-        /* update sensor date */
-        update_sensor(&base->sensor);
-        core_s_t *core = NULL;
-        if ((core = max_temp_core(&base->sensor.cpu_list, core))) {
-                sprintf(str, "%.1f "STR_TEMPERATURE_SIGN, core->input / 1000.0f);
-                g_print("label = %s, %s\n", core->label, str);
-                gtk_label_set_text(GTK_LABEL(base->gui.cpu_sensor_label), str);
-        }
+	/* update sensor date */
+	update_sensor(&base->sensor);
+	core_s_t *core = NULL;
+	if ((core = max_temp_core(&base->sensor.cpu_list, core))) {
+		sprintf(str, "%.1f "STR_TEMPERATURE_SIGN, core->input / 1000.0f);
+		g_print("label = %s, %s\n", core->label, str);
+		gtk_label_set_text(GTK_LABEL(base->gui.cpu_sensor_label), str);
+	}
 
-        g_print("Timeout !!!\n");
+	g_print("Timeout !!!\n");
 
-        return TRUE;
+	return TRUE;
 }
 
 
 void system_monitor_free(XfcePanelPlugin *plugin, sys_monitor_t *base)
 {
-        free_cpu(&base->cpu);
-        free_net(&base->net);
-        free(base->font);
-        free(base);
+	free_cpu(&base->cpu);
+	free_net(&base->net);
+	free(base->font);
+	free(base);
 }
