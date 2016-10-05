b88761a3de600417b58e46922991e8ba57f7ca44
d0u9 <d0u9.su@outlook.com>
2016-10-05 15:20:40 +0800

    Refactor sensor.c and sensor.h

--- sensor.h
+++ sensor.h
@@ -1,32 +1,32 @@
 #ifndef __SENSOR_H
 #define __SENSOR_H
 
-#define MAX_CPU_LABEL_LEN       32
-
 #include "list.h"
 
+#define MAX_CPU_LABEL_LEN	32
+
 struct core_s {
-        list_t  list;
-        int     index;
-        int     input;
-        int     crit;
-        int     max;
-        int     crit_alarm;
-        char    label[MAX_CPU_LABEL_LEN];
+	list_t  list;
+	int  index;
+	int  input;
+	int  crit;
+	int  max;
+	int  crit_alarm;
+	char label[MAX_CPU_LABEL_LEN];
 };
-typedef struct core_s     core_s_t;
+typedef struct core_s core_s_t;
 
 struct cpu_s {
-        list_t  list;
-        list_t  core_list;
-        list_t  *cur_core;
-        int     index;
+	list_t list;
+	list_t core_list;
+	list_t *cur_core;
+	int    index;
 };
-typedef struct cpu_s      cpu_s_t;
+typedef struct cpu_s cpu_s_t;
 
 typedef struct {
-        list_t  cpu_list;
-        list_t  *cur_cpu;
+	list_t cpu_list;
+	list_t *cur_cpu;
 } sensor_t;
 
 
